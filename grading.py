e22355879a297fcd48fd97bb9441b7c3ac0caab2
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2023-02-13 17:15:08 +0300

    feat: create a new python file

---
+++ grading.py
@@ -0,0 +1,20 @@
+def grade_calculator():
+    print("Enter 'q' to quit.")
+    while True:
+        score = input("Enter a score between 0 and 100: ")
+        if score == 'q':
+            break
+        score = int(score)
+        if score >= 90:
+            grade = 'A'
+        elif score >= 80:
+            grade = 'B'
+        elif score >= 70:
+            grade = 'C'
+        elif score >= 60:
+            grade = 'D'
+        else:
+            grade = 'F'
+        print(f"Score: {score}, Grade: {grade}")
+
+grade_calculator()
